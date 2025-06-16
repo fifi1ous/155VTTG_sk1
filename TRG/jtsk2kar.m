d79f2957877ca5bdb6f74c7b218a8e7d0b9a4ac7
fifi1ous <phill.roucka@gmail.com>
2025-06-16 19:11:40 +0200

    XY  to SD

---
+++ TRG/jtsk2kar.m
@@ -0,0 +1,19 @@
+function [S,D,ro1,eps] = jtsk2kar(Y,X)
+    ro = pi/180;
+    fi0 = 49.5*ro;
+    S0 = 78.5*ro;
+    a = 6377397.15508;
+    e2 = 0.006674372230622;
+    e = sqrt(e2);
+    R = a*sqrt(1-e2)/(1-e2*sin(fi0)^2);
+    ro0 = 0.9999*R/tan(S0);
+    alfa = sqrt(1+e2*cos(fi0)^4/(1-e2));
+    U0 = asin(sin(fi0)/alfa);
+    k = (((1-e*sin(fi0))/(1+e*sin(fi0)))^(alfa*e/2)*tan(fi0/2 + pi/4)^alfa)/tan(U0/2 + pi/4);
+    n = sin(S0);
+    Uq = 59.71186025*ro;
+    
+    ro1 = sqrt(X^2+Y^2);
+    eps = atan(Y/X);
+    S = 2*atan((ro0/ro1)^(1/n)*tan(S0/2 + pi/4)) - pi/2; D = eps/sin(S0);
+end
