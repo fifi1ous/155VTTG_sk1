ab900d849085e0e993be0e779881ade01f6be3a8
fifi1ous <phill.roucka@gmail.com>
2025-06-16 09:54:01 +0200

    Division into individual folders

---
+++ TRG/st_1002/LJhz1_14.m
@@ -0,0 +1,30 @@
+% merena data astronomicke orientace na Slunce 
+%         v jedne laboratorni jednotce
+% --------------------------------------------
+% jmeno: Filip Roučka
+% -----------
+% format dat:
+% -----------
+% % hz-smer cile v I. a II.poloze [gon]
+% hzcilm = [gggg.gggg gggg.gggg];
+% % hz-smer a cas SELC Slunce [gon h min s]
+% hzsunm = [
+%   gggg.gggg  hh mm ss.s; %  levy okraj  I.p
+%   gggg.gggg  hh mm ss.s; % pravy okraj II.p
+%   gggg.gggg  hh mm ss.s; %  levy okraj  I.p
+%   gggg.gggg  hh mm ss.s];% pravy okraj II.p
+% --------------------------------------------
+
+% ------------
+% MERENA DATA:
+% ------------
+% hz-smer cile v I. a II.poloze [gon]
+hzcilm = [82.4460 282.4456];
+% hz-smer a cas SELC na Slunce [gon h min s]
+hzsunm = [
+    355.1788  8 2 41.58; %  levy okraj  I.p
+    156.0256  8 3 42.83; % pravy okraj II.p
+    355.6689  8 5  2.35; %  levy okraj  I.p
+    156.6368  8 6 36.04];% pravy okraj II.p
+
+%
---
+++ TRG/st_1002/LJhz2_14.m
@@ -0,0 +1,30 @@
+% merena data astronomicke orientace na Slunce 
+%         v jedne laboratorni jednotce
+% --------------------------------------------
+% jmeno: Filip Roučka
+% -----------
+% format dat:
+% -----------
+% % hz-smer cile v I. a II.poloze [gon]
+% hzcilm = [gggg.gggg gggg.gggg];
+% % hz-smer a cas SELC Slunce [gon h min s]
+% hzsunm = [
+%   gggg.gggg  hh mm ss.s; %  levy okraj  I.p
+%   gggg.gggg  hh mm ss.s; % pravy okraj II.p
+%   gggg.gggg  hh mm ss.s; %  levy okraj  I.p
+%   gggg.gggg  hh mm ss.s];% pravy okraj II.p
+% --------------------------------------------
+
+% ------------
+% MERENA DATA:
+% ------------
+% hz-smer cile v I. a II.poloze [gon]
+hzcilm = [82.4467 282.4465];
+% hz-smer a cas SELC na Slunce [gon h min s]
+hzsunm = [
+    356.7775  8 10 2.25; %  levy okraj  I.p
+    157.6216  8 11 0.53; % pravy okraj II.p
+    357.2259  8 12 5.48; %  levy okraj  I.p
+    157.9592  8 12 33.77];% pravy okraj II.p
+
+%
