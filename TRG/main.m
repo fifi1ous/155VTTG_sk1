818a78e9d6e0f4c0fe6aefcda0ff5c8de51277df
Matěj Klimeš <klimesmatej01@gmail.com>
2025-06-18 16:20:15 +0200

    Add XML parsing to main

--- TRG/main.m
+++ TRG/main.m
@@ -1,12 +1,46 @@
 clc; clear; format long G
 
+%% Načtení a zpracování XML souborů s daty
+% Cesta ke složce se soubory
+folder = 'xml_soubory/';
+files = dir(fullfile(folder, '*.xml'));
 
+% Kontrolní proměnná pro uložení všech výstupů
+data = struct();
+
+for k = 1:length(files)
+    filepath = fullfile(folder, files(k).name);
+    fprintf('Zpracovávám: %s\n', files(k).name);
+    
+    try
+        % Zavolání funkce
+        d = parse_trgsit_xml(filepath);
+        
+        % Extrakce názvu podle stanoviska
+        if isfield(d, 'stanovisko')
+            stan = strtrim(d.stanovisko); % např. '1001'
+            varname = ['data_' stan];        % např. 'data_1001'
+
+            % Dynamické uložení do workspace proměnné
+            data.(varname) = d;
+
+            % uložit jako MAT soubor:
+            % save(fullfile('data', [varname '.mat']), 'd');
+
+        else
+            warning('Soubor %s neobsahuje stanovisko.', files(k).name);
+        end
+    catch ME
+        warning('Chyba při zpracování souboru %s:\n%s', files(k).name, ME.message);
+    end
+end
+disp('Soubory načteny a zpracovány');
+
+%% Centrace směrů
+%% Převod směrů do roviny Křovákova zobrazení
 ss = load("pribl_sour_jtsk.txt");
-
-data = parse_trgsit_xml("trg_format_dat.xml");
-
-
 [S,U,ro1,eps] = jtsk2kar(ss(1),ss(1));
     
 
 r = find(ss(:,1)==1002);
+%%
--- TRG/xml_parse_test.m
+++ TRG/xml_parse_test.m
@@ -1,4 +1,40 @@
 clc; close all; clear variables; format longg;
+% 
+% 
+% data = parse_trgsit_xml('xml_soubory/den1_13_1002_1.xml')
+clc; close all; clear variables; format longg;
 
+% Cesta ke složce se soubory
+folder = 'xml_soubory/';
+files = dir(fullfile(folder, '*.xml'));
 
-data = parse_trgsit_xml('xml_soubory/den1_13_1002_1.xml')
+% Kontrolní proměnná pro uložení všech výstupů
+vystupy = struct();
+
+for k = 1:length(files)
+    filepath = fullfile(folder, files(k).name);
+    fprintf('Zpracovávám: %s\n', files(k).name);
+    
+    try
+        % Zavolání funkce
+        data = parse_trgsit_xml(filepath);
+        
+        % Extrakce názvu podle stanoviska
+        if isfield(data, 'stanovisko')
+            stan = strtrim(data.stanovisko); % např. '1001'
+            varname = ['data_' stan];        % např. 'data_1001'
+
+            % Dynamické uložení do workspace proměnné
+            vystupy.(varname) = data;
+
+            % uložit jako MAT soubor:
+            % save(fullfile('vystupy', [varname '.mat']), 'data');
+
+        else
+            warning('Soubor %s neobsahuje stanovisko.', files(k).name);
+        end
+    catch ME
+        warning('Chyba při zpracování souboru %s:\n%s', files(k).name, ME.message);
+    end
+end
+disp('Hotovo');
